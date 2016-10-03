b57e0f7b0b32f71fbbefaffc556cc5375ca415fd
djmartz77 <djmartz77@yahoo.com>
2016-10-03 15:04:04 -0500

    Added Freeplane Map
    Freeplane Map is the initial design document for this game.

---
+++ Design/Depression Game.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Depression Game" FOLDED="false" ID="ID_90688136" CREATED="1475515210424" MODIFIED="1475524648654" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="" POSITION="right" ID="ID_243079901" CREATED="1475515245144" MODIFIED="1475524643743">
+<edge COLOR="#ff0000"/>
+<node TEXT="Depression" ID="ID_342565684" CREATED="1475505710368" MODIFIED="1475524643742" HGAP_QUANTITY="25.249999664723884 pt" VSHIFT_QUANTITY="0.7499999776482589 pt">
+<node TEXT="Player indicator" ID="ID_1985265167" CREATED="1475509108914" MODIFIED="1475509348473">
+<node TEXT="More depressed" ID="ID_1812969543" CREATED="1475509304178" MODIFIED="1475509309994">
+<node TEXT="Sound becomes muffled" ID="ID_1873701117" CREATED="1475509382391" MODIFIED="1475509395779"/>
+<node TEXT="Color drains from the world" ID="ID_1801895563" CREATED="1475509145198" MODIFIED="1475509419064"/>
+<node TEXT="Harder to act" ID="ID_967691263" CREATED="1475509455172" MODIFIED="1475509466224">
+<node TEXT="Slower movement" ID="ID_226033179" CREATED="1475509469047" MODIFIED="1475509472425"/>
+<node TEXT="stop moving faster" ID="ID_1046935964" CREATED="1475509474331" MODIFIED="1475509502113"/>
+<node TEXT="Can&apos;t do &quot;difficult&quot; things" ID="ID_87016418" CREATED="1475509478170" MODIFIED="1475509486451"/>
+<node TEXT="Don&apos;t hit as hard" ID="ID_221803499" CREATED="1475509503081" MODIFIED="1475509508328"/>
+</node>
+</node>
+<node TEXT="Less depressed" ID="ID_834555525" CREATED="1475509321127" MODIFIED="1475509324390">
+<node TEXT="Color returns" ID="ID_303235543" CREATED="1475509326029" MODIFIED="1475509331886"/>
+<node TEXT="Sound returns to normal" ID="ID_1320194118" CREATED="1475509362945" MODIFIED="1475509401834"/>
+</node>
+</node>
+<node TEXT="Ultimate result if unchecked" ID="ID_413262163" CREATED="1475509161982" MODIFIED="1475509168537">
+<node TEXT="Color drains from the character sprite" ID="ID_694583529" CREATED="1475509424521" MODIFIED="1475509440648"/>
+<node TEXT="Player death" ID="ID_1937730796" CREATED="1475509170279" MODIFIED="1475509173153"/>
+</node>
+<node TEXT="Audible indicator" ID="ID_477735288" CREATED="1475509274939" MODIFIED="1475509279088">
+<node TEXT="Sounds are muffled" ID="ID_1807934764" CREATED="1475509280212" MODIFIED="1475509284124"/>
+</node>
+<node TEXT="How to fix it" ID="ID_466370632" CREATED="1475509534974" MODIFIED="1475509538004">
+<node TEXT="Connection with other characters" ID="ID_1458381006" CREATED="1475509538592" MODIFIED="1475509546511"/>
+<node TEXT="Introspection (How do you represent this?)" ID="ID_1239694881" CREATED="1475509547530" MODIFIED="1475509565139">
+<node TEXT="Can make it worse" ID="ID_1523948292" CREATED="1475509566500" MODIFIED="1475509570541">
+<node TEXT="Particularly if also angry or anxious" ID="ID_1699196452" CREATED="1475509578325" MODIFIED="1475509586791"/>
+</node>
+</node>
+<node TEXT="Creative pursuits" ID="ID_657891298" CREATED="1475509597437" MODIFIED="1475509602620"/>
+<node TEXT="Exercise" ID="ID_1900868604" CREATED="1475509777277" MODIFIED="1475509780129"/>
+<node TEXT="Toughing it out" ID="ID_589268498" CREATED="1475509615952" MODIFIED="1475509624269">
+<node TEXT="Maybe it only lasts for a period of time or a level?" ID="ID_581277249" CREATED="1475509628137" MODIFIED="1475509809015"/>
+</node>
+<node TEXT="Therapy" ID="ID_1514041724" CREATED="1475509646208" MODIFIED="1475509648171">
+<node TEXT="Initially makes it worse" ID="ID_1649939978" CREATED="1475509649876" MODIFIED="1475509655576">
+<node TEXT="&quot;Opening Pandora&apos;s Box&quot;" ID="ID_187860594" CREATED="1475509658332" MODIFIED="1475509668202"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Game Type" POSITION="left" ID="ID_1375628327" CREATED="1475515298184" MODIFIED="1475515302520">
+<edge COLOR="#0000ff"/>
+<node TEXT="2D Platformer" ID="ID_550693771" CREATED="1475515304318" MODIFIED="1475515309835"/>
+</node>
+<node TEXT="Scenes" POSITION="right" ID="ID_1749766028" CREATED="1475515326849" MODIFIED="1475524648653" HGAP_QUANTITY="5.000000268220894 pt" VSHIFT_QUANTITY="40.49999879300598 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="1 - Introduction Cutscene" ID="ID_352732196" CREATED="1475515369475" MODIFIED="1475523216200"/>
+<node TEXT="2 - Introduction to mechanics" ID="ID_1034176580" CREATED="1475523216828" MODIFIED="1475523241619"/>
+<node TEXT="3 - Therapy" ID="ID_877583144" CREATED="1475523242572" MODIFIED="1475523466691"/>
+<node TEXT="4 - Surviving the fall" ID="ID_1809163409" CREATED="1475523471552" MODIFIED="1475523475965"/>
+<node TEXT="5 - Climbing out" ID="ID_1848377904" CREATED="1475523476704" MODIFIED="1475523788267"/>
+</node>
+</node>
+</map>
