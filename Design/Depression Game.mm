a9c1bd2f8a5852929de1404dccb66bdbfad01d13
djmartz77 <djmartz77@yahoo.com>
2016-10-06 08:48:24 -0500

    Shifted things around and added the Wash

--- Design/Depression Game.mm
+++ Design/Depression Game.mm
@@ -2,7 +2,7 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="Depression Game" FOLDED="false" ID="ID_90688136" CREATED="1475515210424" MODIFIED="1475524648654" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle">
+<hook NAME="MapStyle" zoom="1.331">
     <properties fit_to_viewport="false;"/>
 
 <map_styles>
@@ -81,7 +81,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="" POSITION="right" ID="ID_243079901" CREATED="1475515245144" MODIFIED="1475524643743">
 <edge COLOR="#ff0000"/>
 <node TEXT="Depression" ID="ID_342565684" CREATED="1475505710368" MODIFIED="1475524643742" HGAP_QUANTITY="25.249999664723884 pt" VSHIFT_QUANTITY="0.7499999776482589 pt">
@@ -117,30 +117,6 @@
 <node TEXT="Audible indicator" ID="ID_477735288" CREATED="1475509274939" MODIFIED="1475509279088">
 <node TEXT="Sounds are muffled" ID="ID_1807934764" CREATED="1475509280212" MODIFIED="1475509284124"/>
 </node>
-<node TEXT="How to fix it" ID="ID_466370632" CREATED="1475509534974" MODIFIED="1475509538004">
-<node TEXT="Connection with other characters" ID="ID_1458381006" CREATED="1475509538592" MODIFIED="1475509546511"/>
-<node TEXT="Introspection (How do you represent this?)" ID="ID_1239694881" CREATED="1475509547530" MODIFIED="1475509565139">
-<node TEXT="Can make it worse" ID="ID_1523948292" CREATED="1475509566500" MODIFIED="1475509570541">
-<node TEXT="Particularly if also angry or anxious" ID="ID_1699196452" CREATED="1475509578325" MODIFIED="1475509586791"/>
-</node>
-</node>
-<node TEXT="Creative pursuits" ID="ID_657891298" CREATED="1475509597437" MODIFIED="1475509602620"/>
-<node TEXT="Exercise" ID="ID_1900868604" CREATED="1475509777277" MODIFIED="1475509780129"/>
-<node TEXT="Toughing it out" ID="ID_589268498" CREATED="1475509615952" MODIFIED="1475509624269">
-<node TEXT="Maybe it only lasts for a period of time or a level?" ID="ID_581277249" CREATED="1475509628137" MODIFIED="1475509809015"/>
-<node TEXT="ACTING IS REWARDED" ID="ID_1712326605" CREATED="1475613131756" MODIFIED="1475613166024"/>
-<node TEXT="Time heals all things, except when it doesn&apos;t" ID="ID_1900139121" CREATED="1475613167744" MODIFIED="1475613177241">
-<node TEXT="Time can heal you, but works very slowly" ID="ID_1635225263" CREATED="1475613178288" MODIFIED="1475613199822"/>
-<node TEXT="Time is not enough by itself" ID="ID_1792361020" CREATED="1475613200427" MODIFIED="1475613329207"/>
-<node TEXT="You need help from within yourself, outside yourself, or both" ID="ID_810503888" CREATED="1475613351764" MODIFIED="1475613465452"/>
-</node>
-</node>
-<node TEXT="Therapy" ID="ID_1514041724" CREATED="1475509646208" MODIFIED="1475509648171">
-<node TEXT="Initially makes it worse" ID="ID_1649939978" CREATED="1475509649876" MODIFIED="1475509655576">
-<node TEXT="&quot;Opening Pandora&apos;s Box&quot;" ID="ID_187860594" CREATED="1475509658332" MODIFIED="1475509668202"/>
-</node>
-</node>
-</node>
 </node>
 </node>
 <node TEXT="Game Type" POSITION="left" ID="ID_1375628327" CREATED="1475515298184" MODIFIED="1475515302520">
@@ -154,6 +130,88 @@
 <node TEXT="3 - Therapy" ID="ID_877583144" CREATED="1475523242572" MODIFIED="1475523466691"/>
 <node TEXT="4 - Surviving the fall" ID="ID_1809163409" CREATED="1475523471552" MODIFIED="1475523475965"/>
 <node TEXT="5 - Climbing out" ID="ID_1848377904" CREATED="1475523476704" MODIFIED="1475523788267"/>
+</node>
+<node TEXT="making it worse" POSITION="left" ID="ID_1084929636" CREATED="1475758977470" MODIFIED="1475760941536">
+<edge COLOR="#7c0000"/>
+<node TEXT="External Influences" ID="ID_240942525" CREATED="1475760958324" MODIFIED="1475760962586">
+<node TEXT="The Wash - A current, or maybe smoke which is visible on the screen. It increases depression while you are in it." ID="ID_1987071277" CREATED="1475759032260" MODIFIED="1475759061293">
+<node TEXT="You have to keep out of it to let time heal you" ID="ID_757009955" CREATED="1475759062420" MODIFIED="1475759072485">
+<node TEXT="&quot;I&apos;m a leaf on the wind, look how I soar&quot;" ID="ID_663365719" CREATED="1475759148059" MODIFIED="1475759160423"/>
+</node>
+<node TEXT="sometimes the only way out is through" ID="ID_1756228567" CREATED="1475759073278" MODIFIED="1475759080076"/>
+<node TEXT="It can change size/shape/intensity based on character action (memories, inaction, connection, etc.)" ID="ID_1615653394" CREATED="1475759083374" MODIFIED="1475759131807"/>
+<node TEXT="NPC may be stuck in The Wash" ID="ID_272241396" CREATED="1475759132580" MODIFIED="1475759141633">
+<node TEXT="Helping them can help you." ID="ID_1418702309" CREATED="1475759304893" MODIFIED="1475759317821"/>
+<node TEXT="If they don&apos;t want to be helped, you can get sucked in with them" ID="ID_551197200" CREATED="1475759318659" MODIFIED="1475759340294"/>
+</node>
+<node TEXT="Fireflies in the Wash will help you find your way" ID="ID_1205078869" CREATED="1475760776408" MODIFIED="1475760804103"/>
+</node>
+<node TEXT="Memories - Objects in the game world which remind you of bad things" ID="ID_1916840488" CREATED="1475759004233" MODIFIED="1475759029103"/>
+</node>
+</node>
+<node TEXT="making it better" POSITION="left" ID="ID_466370632" CREATED="1475509534974" MODIFIED="1475760950374">
+<edge COLOR="#00007c"/>
+<node TEXT="External Influences" ID="ID_1961862260" CREATED="1475760973479" MODIFIED="1475760979064">
+<node TEXT="Therapy" ID="ID_1514041724" CREATED="1475509646208" MODIFIED="1475509648171">
+<node TEXT="Things get worse before they get better" ID="ID_1649939978" CREATED="1475509649876" MODIFIED="1475761609661">
+<node TEXT="&quot;Opening Pandora&apos;s Box&quot;" ID="ID_187860594" CREATED="1475509658332" MODIFIED="1475509668202">
+<node TEXT="This is probably better as a level rather than a mechanic" ID="ID_880020189" CREATED="1475761613544" MODIFIED="1475761621752"/>
+</node>
+</node>
+<node TEXT="Initially not much effect" ID="ID_1882201932" CREATED="1475761570286" MODIFIED="1475761575567">
+<node TEXT="You are reaching out, so there should be some effect from that" ID="ID_553138851" CREATED="1475761579915" MODIFIED="1475761590788"/>
+</node>
+</node>
+<node TEXT="Time heals all things, except when it doesn&apos;t" ID="ID_1900139121" CREATED="1475613167744" MODIFIED="1475613177241">
+<node TEXT="Time can heal you, but works very slowly" ID="ID_1635225263" CREATED="1475613178288" MODIFIED="1475613199822"/>
+<node TEXT="Time is not enough by itself" ID="ID_1792361020" CREATED="1475613200427" MODIFIED="1475613329207"/>
+<node TEXT="You need help from within yourself, outside yourself, or both" ID="ID_810503888" CREATED="1475613351764" MODIFIED="1475613465452"/>
+</node>
+<node TEXT="Landmarks" ID="ID_375466801" CREATED="1475761056809" MODIFIED="1475761062691">
+<node TEXT="Sometimes you just know you&apos;re getting better because you see something differently" ID="ID_1995033722" CREATED="1475761063884" MODIFIED="1475761084131">
+<node TEXT="&quot;I haven&apos;t noticed anything beautiful in a long time&quot;" ID="ID_208348648" CREATED="1475761085620" MODIFIED="1475761098957"/>
+<node TEXT="Gives you a boost" ID="ID_418745610" CREATED="1475761104571" MODIFIED="1475761110164"/>
+<node TEXT="Feeling pain is a double-edged sword." ID="ID_717369813" CREATED="1475761217242" MODIFIED="1475761235433">
+<node TEXT="It is a coping mechanism" ID="ID_895755686" CREATED="1475761244847" MODIFIED="1475761262458"/>
+<node TEXT="It makes you less resistant to death by depression" ID="ID_964369522" CREATED="1475761263107" MODIFIED="1475761289027"/>
+<node TEXT="DO I INCLUDE THIS?!" ID="ID_1501191530" CREATED="1475761298668" MODIFIED="1475761310808">
+<node TEXT="If I&apos;m being honest, it&apos;s important to the experience of many" ID="ID_235811335" CREATED="1475761311874" MODIFIED="1475761323475"/>
+<node TEXT="Is this in poor taste?" ID="ID_1513095102" CREATED="1475761324028" MODIFIED="1475761338979">
+<node TEXT="Does that matter?" ID="ID_1824104334" CREATED="1475761340119" MODIFIED="1475761345323"/>
+</node>
+<node TEXT="Does seeing this encourage people?" ID="ID_1615467672" CREATED="1475761360974" MODIFIED="1475761422133">
+<node TEXT="Maybe I need to consult with someone." ID="ID_1963714886" CREATED="1475761372831" MODIFIED="1475761426818"/>
+<node TEXT="Maybe it doesn&apos;t matter if its use is costly enough." ID="ID_772437022" CREATED="1475761380289" MODIFIED="1475761403311">
+<node TEXT="How costly is it? Maybe I need to consult with someone." ID="ID_581603051" CREATED="1475761405349" MODIFIED="1475761418628"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Memories - Objects in the world which remind you of good things" ID="ID_1604743735" CREATED="1475761561741" MODIFIED="1475761645680">
+<node TEXT="Can also be brought up by NPCs near you. Shared positive/meaningful experience" ID="ID_163748386" CREATED="1475761646508" MODIFIED="1475761664638"/>
+</node>
+</node>
+<node TEXT="Internal influences" ID="ID_1229559581" CREATED="1475761476217" MODIFIED="1475761480151">
+<node TEXT="Toughing it out" ID="ID_589268498" CREATED="1475509615952" MODIFIED="1475509624269">
+<node TEXT="Maybe it only lasts for a period of time or a level?" ID="ID_581277249" CREATED="1475509628137" MODIFIED="1475509809015"/>
+<node TEXT="ACTING IS REWARDED" ID="ID_1712326605" CREATED="1475613131756" MODIFIED="1475613166024">
+<node TEXT="Maybe forward progress, percentage progress, raw action of any kind or some combination can be helpful?" ID="ID_1989655519" CREATED="1475761027874" MODIFIED="1475761049336"/>
+</node>
+</node>
+<node TEXT="Exercise" ID="ID_1900868604" CREATED="1475509777277" MODIFIED="1475509780129"/>
+<node TEXT="Creative pursuits" ID="ID_657891298" CREATED="1475509597437" MODIFIED="1475509602620">
+<node TEXT="Crafting system?" ID="ID_1948408050" CREATED="1475761496471" MODIFIED="1475761507401"/>
+<node TEXT="Mini-games?" ID="ID_1271858625" CREATED="1475761508023" MODIFIED="1475761512213"/>
+</node>
+<node TEXT="Introspection (How do you represent this?)" ID="ID_1239694881" CREATED="1475509547530" MODIFIED="1475509565139">
+<node TEXT="Can make it worse" ID="ID_1523948292" CREATED="1475509566500" MODIFIED="1475509570541">
+<node TEXT="Particularly if also angry or anxious" ID="ID_1699196452" CREATED="1475509578325" MODIFIED="1475509586791"/>
+</node>
+</node>
+<node TEXT="Connection with other characters" ID="ID_1458381006" CREATED="1475509538592" MODIFIED="1475509546511"/>
+</node>
 </node>
 </node>
 </map>
